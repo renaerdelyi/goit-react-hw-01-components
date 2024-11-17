4e3c1d0ced03c49fa1eb96f8d55be3b5ce2f8ae8
renaerdelyi <rena.szabo@yahoo.com>
2024-11-17 07:26:55 +0200

    add3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Profile from './Profile';
-import PropTypes from 'prop-types';
+
 
 function App() {
   const user = {
-      "username": "Jacques Gluke",
-      "tag": "jgluke",
-      "location": "Ocho Rios, Jamaica",
-      "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-      "stats": {
-        "followers": 5603,
-        "views": 4827,
-        "likes": 1308
-      }
-  }
+    username: 'Jacques Gluke',
+    tag: 'jgluke',
+    location: 'Ocho Rios, Jamaica',
+    avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+    stats: {
+      followers: 5603,
+      views: 4827,
+      likes: 1308,
+    },
+  };
   return (
     <div
       style={{
@@ -25,10 +25,16 @@ function App() {
         color: '#010101',
       }}
     >
-      <Profile username={items.name} tag={items.tag} location={items.location} avatar={items.avatar} stats={items.stats}/>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 }
 
 export default App;
-console.log(PropTypes);
+
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,34 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Profile(items) {
+function Profile({username, tag, location, avatar, stats}) {
   return (
     <div class="profile">
       <div class="description">
         <img
-          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-          alt="User avatar"
-          class="avatar"
-        />{items.avatar}
-        <p class="name">{items.name}</p>
-        <p class="tag">{items.tag}</p>
-        <p class="location">{items.location}</p>
+          src={avatar}
+          alt={`${username}'s avatar`}
+          className="avatar"
+        />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
       </div>
 
-      <ul class="stats">{items.stats}
+      <ul class="stats">
         <li>
           <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span class="quantity">{stats.followers}</span>
         </li>
         <li>
           <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span class="quantity">{stats.views}</span>
         </li>
         <li>
           <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span class="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
+
+Profile.propTypes = {
+    usename: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats:PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }).isRequired,
+};
+
 export default Profile;
